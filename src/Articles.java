8181cefee7488735fcf8c9fc9be9813a406bf4eb
Kaia Tomson <kaiatoms@gmail.com>
2015-12-14 19:46:36 +0200

    1. commit

--- src/Articles.java
+++ src/Articles.java
@@ -1,23 +1,36 @@
+import java.applet.Applet;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 /**
  * Created by mpurola on 18.10.15.
  */
-public class Articles {
+public class Articles  {
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
 
         String[] articles = new String[]  {"Der", "Die", "Das"};
 
         Map<String,String> wordArticlesPairs = new HashMap<String, String>();
 
-        wordArticlesPairs.put("Bruder", articles[0]);
-        wordArticlesPairs.put("Vater", articles[0]);
-        wordArticlesPairs.put("Blume", articles[1]);
-        wordArticlesPairs.put("Mutter", articles[1]);
-        wordArticlesPairs.put("Kind", articles[2]);
-        wordArticlesPairs.put("Haus", articles[2]);
+        BufferedReader br = null;
 
+        try {
+            br = new BufferedReader(new FileReader("/Users/kaiatomson/Desktop/Programmeerimine/DerDIeDasGame2/src/db"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open file!");
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(br.readLine());
+            wordArticlesPairs.put(br.readLine(), br.readLine());
+        }
 
         Iterator<String> articlesIterator = wordArticlesPairs.keySet().iterator();
 
@@ -30,22 +43,23 @@ public class Articles {
                 System.out.println(i + ": " + articles[i]);
             }
 
-            while(true) {
 
-                System.out.print("Write the corrent number to select article: ");
+        while(true) {
+
+                System.out.print("Write the correct number to select article: ");
 
                 int answer = TextIO.getlnInt();
 
-                if (answer >= articles.length || answer < 0) {
+                /*if (answer) {
                     System.out.println("Please try again");
                     continue;  // jump back beginning of this while loop
-                }
+                }*/
 
                 if (articles[answer].equals(wordArticlesPairs.get(sona))) {
                     System.out.println("Correct!");
                     break;  // exits this while loop
                 } else {
-                    System.out.println("Wrong!");
+                    System.out.println("Wrong! Try again");
                 }
             }
         }
